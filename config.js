55a1363d3575fc9f6e9cfb7736189e21e91a2c8b
Glitch (steep-magnificent-wound) <none>
2020-12-28 19:00:27 +0000

    🎳💵 Checkpoint ./komutlar/avatar.js:26015749/20 ./config.js:26015749/464

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "token": "Botunuzun tokeni",
-  "geliştiriciler": ["Kendi Discord ID'nizi buraya yazın."], 
-  "prefix": "Botunuzun prefixi."
+  token: "NzkxNjExNDM1NzkyNTk3MDAy.X-RrmQ.wTZ54Vc0_nNQomz1u6yeM_2klbw",
+  geliştiriciler: ["720358716670476408"],
+  prefix: "!!"
 };

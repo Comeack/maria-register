f39b9512aba48d5697f215640410dd9eb814779d
Glitch (steep-magnificent-wound) <none>
2020-12-28 18:50:08 +0000

    Import of aru-xyz/discord-bos-v12 ./komutlar/avatar.js:26015749/20

--- events/ready.js
+++ events/ready.js
@@ -1,25 +1,17 @@
-const chalk = require("chalk");
-const moment = require("moment");
 const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json");
-
-var prefix = ayarlar.prefix;
-
-module.exports = client => {
-  console.log(
-    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
-  );
-  console.log(
-    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
-      client.user.username
-    } ismi ile giriş yapıldı!`
-  );
-  client.user.setStatus("dnd");
-  var oyun = ["Comeback ❤️"];
-
-  setInterval(function() {
-    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
-
-    client.user.setActivity(oyun[random], "");
-  }, 2 * 2500);
+const config = require('../config.js');
+module.exports = async client => {
+  client.user.setPresence({ activity: { type: "WATCHING", name: `Piece ❤ Serendia Squad`}, status: 'idle' })
 };
+
+// Type kısımları:
+// WATCHING - İZLİYOR
+// PLAYING - OYNUYOR
+// LISTENING - DİNLİYOR
+
+// Status kısımları:
+// online - çevrim içi
+// idle - boşta
+// dnd - rahatsız etmeyin
+
+// name kısmına oynuyorunuzu yazabilirsiniz.

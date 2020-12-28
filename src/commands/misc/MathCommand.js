3b672927a868f5d0901eca2d45641ec79a62d85b
Glitch (steep-magnificent-wound) <none>
2020-12-28 20:03:25 +0000

    🌲🏘 Checkpoint ./src/commands/misc/MathCommand.js:26015749/2766

--- src/commands/misc/MathCommand.js
+++ src/commands/misc/MathCommand.js
@@ -1,26 +1,31 @@
-const BaseCommand = require('../../utils/structures/BaseCommand');
+const BaseCommand = require("../../utils/structures/BaseCommand");
 
 module.exports = class MathCommand extends BaseCommand {
   constructor() {
-    super('math', 'misc', ['calc', 'calculator'], 'math [number] [+ | - | * | /] [number]');
+    super(
+      "math",
+      "misc",
+      ["calc", "calculator"],
+      "math [number] [+ | - | * | /] [number]"
+    );
   }
 
   run(client, message, args) {
-    if(!args[0]) return message.reply('Enter a first number'); //if there is no first arg
-    if(!args[1]) return message.reply('Enter an operator'); //if there is no second arg
-    if(!args[2]) return message.reply('Enter a second number'); //if there is no third arg
+    if (!args[0]) return message.reply("Enter a first number"); //if there is no first arg
+    if (!args[1]) return message.reply("Enter an operator"); //if there is no second arg
+    if (!args[2]) return message.reply("Enter a second number"); //if there is no third arg
 
     //send the result of calculator to the channel
     message.channel.send(calculator(args[0], args[1], args[2]));
   }
-}
+};
 
-function calculator(num1, operator, num2){
-  if(isNaN(num1)) return 'Number 1 is not a number'; //if the first number is not a number return an error message
-  if(isNaN(num2)) return 'Number 2 is not a number'; //if the second number is not a number return an error message
+function calculator(num1, operator, num2) {
+  if (isNaN(num1)) return "Number 1 is not a number"; //if the first number is not a number return an error message
+  if (isNaN(num2)) return "Number 2 is not a number"; //if the second number is not a number return an error message
 
   //switch through the allowed operators and perform the specified operations
-  switch(operator){
+  switch (operator) {
     case "+":
       return parseInt(num1) + parseInt(num2);
       break;

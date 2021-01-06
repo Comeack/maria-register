66ab6252b330ae4c9c1d971415fabf9bcd80fbc9
Glitch (steep-magnificent-wound) <none>
2021-01-06 09:47:34 +0000

    🐩🏪 Checkpoint ./events/ready.js:26015749/1366

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,20 @@
-const chalk = require('chalk');
-const moment = require('moment');
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
 
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
+  );
+  console.log(
+    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
+      client.user.username
+    } ismi ile giriş yapıldı!`
+  );
   client.user.setStatus("online");
-  client.user.setActivity("Helia Register en iyi şekilde hizmet veriyor. Helia sunucusunda sorunsuz bir şekilde kayit yapıyorum.", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
+  client.user.setActivity("Helia ❤️ Comeback", { type: "WATCHING" }); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
   console.log(``);
-
 };

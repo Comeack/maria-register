253a1bcab269524c56d899e11f4e9e436017b94f
Glitch (steep-magnificent-wound) <none>
2021-01-11 20:06:03 +0000

    🌓😚 Checkpoint ./komutlar/kayıt-erkek.js:26015749/218

--- komutlar/kayıt-erkek.js
+++ komutlar/kayıt-erkek.js
@@ -7,6 +7,7 @@ if(!["798090394562330636"].some(role => message.member.roles.cache.get(role)) &&
   
 const erkek = message.guild.roles.cache.find(r => r.id === "798257045442527272")
 const xy = message.guild.roles.cache.find(r => r.id === "798257044301152257")
+//const xy = message.guild.roles.cache.find(r => r.id === "798257044301152257")
 const kayıtsız = message.guild.roles.cache.find(r => r.id === "798257046914334731")
 const reglog = message.guild.channels.cache.find(c => c.id === "798257139944652851")
 const genelchat = message.guild.channels.cache.find(g => g.id === "798090500191944724")
@@ -39,7 +40,9 @@ x.roles.add(xy)
 x.roles.remove(kayıtsız)
 
 
-genelchat.send(`<@${member.id}> **Aramıza Hoş Geldin.** <a:kalp_1:798150464754548746>`)
+genelchat.send(`
+<@${member.id}> **Aramıza Hoş Geldin.** <a:kalp_1:798150464754548746>
+`)
 
 }
 

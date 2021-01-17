5002fad8dbc6a41a7d64279275080bac9a52248e
Glitch (steep-magnificent-wound) <none>
2021-01-17 10:54:20 +0000

    🐠💎 Checkpoint ./komutlar/deneme.js:26015749/268 ./komutlar/kayıt-topteyit.js:26015749/2720 ./komutlar/kayıt-sıfırla.js:26015749/3465 ./komutlar/kayıt-erkek.js:26015749/9

--- komutlar/kayıt-erkek.js
+++ komutlar/kayıt-erkek.js
@@ -1,63 +1,82 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
+const { MessageEmbed } = require('discord.js')
+const datab = require('quick.db')
+const ms = require('ms')
+const moment = require("moment");
+const { parseZone } = require("moment");
 
-exports.run = async (client, message, args) => {
+exports.run =  async (client, message, args) => {
   
-if(!["798257016291459083"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) return message.channel.send(`Bu Komutu Kullanabilmek İçin Yetkin Bulunmuyor.`)
+if(!['798257016291459083'].some(role => message.member.roles.cache.get(role)) && !message.member.hasPermission('ADMINISTRATOR')) 
+return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`${message.author} **Bu komutu kullanmak için yetkin bulunmamakta.**`)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
   
-const erkek = message.guild.roles.cache.find(r => r.id === "798257045442527272")
-const xy = message.guild.roles.cache.find(r => r.id === "798257044301152257")
-const kayıtsız = message.guild.roles.cache.find(r => r.id === "798257046914334731")
-const reglog = message.guild.channels.cache.find(c => c.id === "798257160391360542")
-const genelchat = message.guild.channels.cache.find(g => g.id === "798257160391360542")
+const tag = 'र'   
+const erkekrol = message.guild.roles.cache.find(r => r.id === '798257045442527272') 
+const erkekrol2 = message.guild.roles.cache.find(r => r.id === '798257044301152257')
+const kayıtsız = message.guild.roles.cache.find(r => r.id === '798257046914334731')
+const genelchat = message.guild.channels.cache.find(c => c.id === '798257160391360542')
+const savelog = message.guild.channels.cache.find(c => c.id === '800310637922091048')
+
 
 const member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
-if(!member) return message.channel.send(`Bir Kullanıcı Belirt.`)
-if(!member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send(`Bu Kullanıcı Sizle Üst/Aynı Pozisyondadır.`)
-const x = message.guild.member(member)
+if(!member) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`${message.author} **bir kullanıcı belirt.**`)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
+let name = args[1]
+let age = Number(args[2])
+if(!name) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`\`Bir isim belirtmelisin.\``)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
+if(!age) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`\`Bir yaş belirtmelisin.\``)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
+if(member.id === message.author.id) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`\`Kendini kayıt edemezsin.\``)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
+if(member.id === client.user.id) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`\`Bot kayıt edemezsin.\``)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
+if(member.id === message.guild.OwnerID) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`\`Sunucu sahibini kayıt edemezsin.\``)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
+if(member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`\`Belirtilen kullanıcı sizden üst/aynı pozisyonda işleme devam edilemiyor.\``)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
+  
+  
+datab.add(`yetkili.${message.author.id}.kadin`, 1)
+datab.add(`yetkili.${message.author.id}.toplam`, 1)
+let alldata = datab.fetch(`yetkili.${message.author.id}.toplam`)
 
-let tag = "र"
-let isim = args[1]
-let yas = Number(args[2])
-if(!isim) return message.channel.send(`Bir İsim Belirt`)
-if(!yas) return message.channel.send(`Bir Yaş Belirt`)
-
-let bilgi = db.get(`yetkili.${member.id}`);  
-db.add(`yetkili.${message.author.id}.erkek`,1 )
-db.add(`yetkili.${message.author.id}.toplam`, 1)  
-let toplami = db.fetch(`yetkili.${message.author.id}.toplam`)  
-
-message.react('✅')
-x.setNickname(`${tag} ${isim} | ${yas}`)
-x.roles.add(erkek)
-x.roles.add(xy)
-x.roles.remove(kayıtsız)
-//
-x.setNickname(`${tag} ${isim} | ${yas}`)
-x.roles.add(erkek)
-x.roles.add(xy)
-x.roles.remove(kayıtsız)
-
-
-genelchat.send(`
-<@${member.id}> **Aramıza Hoş Geldin.** <a:kalp_1:798150464754548746>
-`)
-
-}
-
-exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ["erkek", "e", "man", "boy"],
-    permLevel: 0
-};
-
-exports.help = {
-    name: "erkek"
-}
+member.setNickname(`${tag} ${name} | ${age}`)
+member.roles.add(erkekrol)
+member.roles.add(erkekrol2)
+member.roles.remove(kayıtsız)
 
 
+message.channel.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`${member} **Üyesini** ${message.author} **Teyit Etti.** \n\n ${erkekrol}, ${erkekrol2} **Rolleri Verildi.** \n \`${tag} ${name} | ${age}\` **Olarak ismi güncellendi.**`)
+.setFooter(`Toplam kayıtların: ${alldata}`)               
+.setColor('#2e042c'))
+  
+genelchat.send(`<@${member.id}> **Aramıza Hoş Geldin.** <a:kalp_1:798150464754548746>`)
+  
+savelog.send(new MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+.setDescription(`<a:maria__tac:798886561428275211> **Yetkili:** ${message.author} | \`${message.author.id}\`\n<a:maria__tac:798886561428275211> **Kullanıcı:** ${member} | \`${member.id}\`\n<a:maria__tac:798886561428275211> **Güncel İsim:** \`${tag} ${name} | ${age}\`\n<a:maria__tac:798886561428275211> Roller: ${erkekrol}, ${erkekrol2} \n<a:maria__tac:798886561428275211> **Kanal:** <#${message.channel.id}> | \`${message.channel.id}\`\n<a:maria__tac:798886561428275211> **Kayıtlar:** \`${alldata}\` `)
+.setColor('#2e042c'))
 
 
-
-
+datab.push(`isim.${message.guild.id}`, {userID: member.id, isim: name, yas: age, role: erkekrol.id})}
+exports.conf = {enabled: true, guildOnly: true, aliases: ["erkek"], permLevel: 0}
+exports.help = {name: 'erkek', description: "Etiketlenen kişiyi erkek rolleriyle kayıt eder.", usage: '.erkek @etiket/id İsim Yaş'}
--- komutlar/kayıt-sıfırla.js
+++ komutlar/kayıt-sıfırla.js
@@ -3,22 +3,36 @@ const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
   
-if(!["798257001237970965"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) return message.channel.send(`Bu Komutu Kullanabilmek İçin Yetkin Bulunmuyor.`)
+if(!["798257001237970965"].some(role => message.member.roles.cache.get(role)) && (!message.member.hasPermission("ADMINISTRATOR"))) 
+return message.channel.send(new Discord.MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`${message.author} **bu komutu kullanmak için yetkin bulunmamakta.**`)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
 
 
 const member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
-if(!member) return message.channel.send(`Bir Kullanıcı Belirt.`)
-if(!member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send(`Bu Kullanıcı Sizle Üst/Aynı Pozisyondadır.`)
+if(!member) return message.channel.send(new Discord.MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`**Bir kullanıcı belirt.**`)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
+if(!member.roles.highest.position >= message.member.roles.highest.position) message.channel.send(new Discord.MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setDescription(`**Belirtilen kullanıcı sizden üst/aynı pozisyonda.**`)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
 
   
 let bilgi = db.get(`yetkili.${member.id}`);  
 db.delete(`yetkili.${message.author.id}.erkek`)
 db.delete(`yetkili.${message.author.id}.toplam`)  
+db.delete(`yetkili.${message.author.id}.kadin`)
 let toplami = db.fetch(`yetkili.${message.author.id}.toplam`)  
 
 message.react('✅')
 
-message.channel.send(new Discord.MessageEmbed().setAuthor(`Kayıt Sıfırlandı`).setColor("BLACK").setDescription(`${member} Adlı Kullanıcının Db'si Silindi, <@${message.author.id}> Tarafından Sıfırlandı.`))
+message.channel.send(new Discord.MessageEmbed()
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
+.setColor("#2e042c")
+.setDescription(`${member} **Adlı Kullanıcının Kayıtları Silindi,** <@${message.author.id}> **Tarafından Sıfırlandı.**`))
   
 
 }

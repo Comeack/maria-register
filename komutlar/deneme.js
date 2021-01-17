b1592de2c1c972822430cc61921dffbbfa803361
Glitch (steep-magnificent-wound) <none>
2021-01-17 10:31:58 +0000

    📷🚟 Checkpoint ./komutlar/deneme.js:26015749/1302 ./bot.js:26015749/14

--- komutlar/deneme.js
+++ komutlar/deneme.js
@@ -6,31 +6,19 @@ const { parseZone } = require("moment");
 
 exports.run =  async (client, message, args) => {
   
-if(!['YETKİLİ ROL ID'].some(role => message.member.roles.cache.get(role)) && !message.member.hasPermission('ADMINISTRATOR')) 
+if(!['798257016291459083'].some(role => message.member.roles.cache.get(role)) && !message.member.hasPermission('ADMINISTRATOR')) 
 return message.channel.send(new MessageEmbed()
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
-.setDescription(`${message.author} bu komutu kullanmak için yetkin bulunmamakta.`)
-.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
+.setDescription(`${message.author} **Bu komutu kullanmak için yetkin bulunmamakta.**`)
+.setColor('#2e042c')).then(x => x.delete({timeout: 5000}));
   
-const tag = 'STRG'   
-const erkekrol = message.guild.roles.cache.find(r => r.id === '') 
-const erkekrol2 = message.guild.roles.cache.find(r => r.id === '')
-const kayıtsız = message.guild.roles.cache.find(r => r.id === '')
-const genelchat = message.guild.channels.cache.find(c => c.id === '')
-const savelog = message.guild.channels.cache.find(c => c.id === '')
+const tag = 'र'   
+const erkekrol = message.guild.roles.cache.find(r => r.id === '798257045442527272') 
+const xy = message.guild.roles.cache.find(r => r.id === '798257044301152257')
+const kayıtsız = message.guild.roles.cache.find(r => r.id === '798257046914334731')
+const genelchat = message.guild.channels.cache.find(c => c.id === '798257160391360542')
+const savelog = message.guild.channels.cache.find(c => c.id === '800310637922091048')
 
-if(!erkekrol) return message.channel.send(new MessageEmbed()
-.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
-.setDescription(`1. Erkek rolü ayarlanmamış/yanlış id girilmiş kayıt işlemine devam edilemiyor.`)
-.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
-if(!erkekrol2) return message.channel.send(new MessageEmbed()
-.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
-.setDescription(`2. Erkek rolü ayarlanmamış/yanlış id girilmiş kayıt işlemine devam edilemiyor.`)
-.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
-if(!kayıtsız) return message.channel.send(new MessageEmbed()
-.setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
-.setDescription(`Kayıtsız rolü ayarlanmamış/yanlış id girilmiş kayıt işlemine devam edilemiyor.`)
-.setColor('#a22a2a')).then(x => x.delete({timeout: 5000}));
 
 const member = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
 if(!member) return message.channel.send(new MessageEmbed()
@@ -71,13 +59,13 @@ let alldata = datab.fetch(`yetkili.${message.author.id}.toplam`)
 
 member.setNickname(`${tag} ${name} | ${age}`)
 member.roles.add(erkekrol)
-member.roles.add(erkekrol2)
+member.roles.add(xy)
 member.roles.remove(kayıtsız)
 
 
 message.channel.send(new MessageEmbed()
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic:true}))
-.setDescription(`${member} üyesini ${message.author} kayıt etti. \n\n ${erkekrol}, ${erkekrol2} Rolleri Verildi. \n \`${tag} ${name} | ${age}\` Olarak ismi güncellendi.`)
+.setDescription(`${member} üyesini ${message.author} kayıt etti. \n\n ${erkekrol}, ${xy} Rolleri Verildi. \n \`${tag} ${name} | ${age}\` Olarak ismi güncellendi.`)
 .setFooter(`Toplam kayıtların: ${alldata}`)               
 .setColor('#65d8c4'))
   
@@ -86,7 +74,7 @@ Sunucumuzun **${tag}** tagını alarak ailemizin parçası olabilirsin.`)
   
 savelog.send(new MessageEmbed()
 .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
-.setDescription(`• Yetkili: ${message.author} | \`${message.author.id}\`\n• Kullanıcı: ${member} | \`${member.id}\`\n• Güncel İsim: \`${tag} ${name} | ${age}\`\n• Roller: ${erkekrol}, ${erkekrol2} \n• Kanal: <#${message.channel.id}> | \`${message.channel.id}\`\n• Kayıtlar: \`${alldata}\` `)
+.setDescription(`• Yetkili: ${message.author} | \`${message.author.id}\`\n• Kullanıcı: ${member} | \`${member.id}\`\n• Güncel İsim: \`${tag} ${name} | ${age}\`\n• Roller: ${erkekrol}, ${xy} \n• Kanal: <#${message.channel.id}> | \`${message.channel.id}\`\n• Kayıtlar: \`${alldata}\` `)
 .setColor('#65d8c4'))
 
 

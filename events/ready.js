eac63d5704e3fd746a01f9d57eb2eb41d6e673f0
Glitch (steep-magnificent-wound) <none>
2021-01-21 18:51:57 +0000

    🚨🧀 Checkpoint ./events/ready.js:26015749/4878

--- events/ready.js
+++ events/ready.js
@@ -1,20 +1,9 @@
-const chalk = require("chalk");
-const moment = require("moment");
-const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json");
+const ayarlar = require('../ayarlar.json');
 
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(
-    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Aktif, Komutlar yüklendi!`
-  );
-  console.log(
-    `[${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
-      client.user.username
-    } maria siker`
-  );
-  client.user.setStatus("online");
-  client.user.setActivity("MAЯIΛ REGİSTER", { type: "WATCHING" }); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
-  console.log(`MARİA BOT AKTİF`);
+  console.log(`BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("dnd");
+  client.user.setActivity('MAЯIΛ REGİSTER', { type: 'WATCHING' });
 };

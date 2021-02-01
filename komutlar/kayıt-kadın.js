bf58528eda1b760db880c49d7dd6ed15c0b98355
Glitch (steep-magnificent-wound) <none>
2021-02-01 18:24:32 +0000

    🍶🐸 Checkpoint ./komutlar/isimler.js:3505607/1310:26015749/15 ./komutlar/kayıt-kadın.js:3505607/130 ./komutlar/isim.js:3505607/2924:26015749/282

--- komutlar/kayıt-kadın.js
+++ komutlar/kayıt-kadın.js
@@ -166,12 +166,7 @@ exports.run = async (client, message, args) => {
       .setColor("#2e042c")
   );
 datab.push(`isimler_${member.id}`, ` \`${name} | ${age}\` (<@&798257043906494515>)`);
-  datab.push(`isim.${message.guild.id}`, {
-    userID: member.id,
-    isim: name,
-    yas: age,
-    role: kadınrol.id
-  });
+ 
 };
 exports.conf = {
   enabled: true,
